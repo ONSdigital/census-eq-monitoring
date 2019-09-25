a389b7c007be121c2369a9d8c038e9aa42b8f0fa
James Gardiner <james@jgardiner.co.uk>
2019-09-25 09:37:49 +0100

    Make project id mandatory

--- create.sh
+++ create.sh
@@ -7,21 +7,17 @@ if [[ -z "$ENV" ]]; then
   exit 1
 fi
 
+if [[ -z "$EXISTING_PROJECT_ID" ]]; then
+  echo "Missing EXISTING_PROJECT_ID variable"
+  exit 1
+fi
+
 ENV=$1
 TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-census-eq-monitoring-tfstate}"
 
 terraform init --upgrade --backend-config prefix=${ENV} --backend-config bucket=${TERRAFORM_STATE_BUCKET}
 
-if [ ! -z $EXISTING_PROJECT_ID ]; then
-    echo "Using existing project_id: $EXISTING_PROJECT_ID"
-
-    if terraform state list | grep -q "google_project.project"; then
-        echo "State contains a google project. Not importing"
-    else
-        echo "State does not contain a google project. Importing $EXISTING_PROJECT_ID"
-        terraform import google_project.project $EXISTING_PROJECT_ID
-    fi
-fi
+terraform import google_project.project $EXISTING_PROJECT_ID
 
 # Roll out infrastructure
 terraform apply -auto-approve
