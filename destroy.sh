36608689058f72bf6cdb940e672fb600b837bd10
James Gardiner <james@jgardiner.co.uk>
2019-10-03 15:59:25 +0100

    Fix destroy script

--- destroy.sh
+++ destroy.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-ENV=$1
+if [[ -z "$ENV" ]]; then
+  echo "Missing ENV variable"
+  exit 1
+fi
+
 TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-census-eq-monitoring-tfstate}"
 
 terraform init --upgrade --backend-config prefix=${ENV} --backend-config bucket=${TERRAFORM_STATE_BUCKET}
