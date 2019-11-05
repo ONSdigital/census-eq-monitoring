a36ebd066c89273a1e289e9c569ee5037218fdd7
Andrew Maddaford <ajmaddaford@gmail.com>
2019-11-05 11:43:00 +0000

    Add uptime check (#4)

--- monitoring_alerts.tf
+++ monitoring_alerts.tf
@@ -10,6 +10,67 @@ resource "google_monitoring_notification_channel" "slack_alert" {
   }
 }
 
+resource "google_monitoring_uptime_check_config" "http" {
+  project      = "${var.stackdriver_workspace}"
+  display_name = "${var.uptime_check_host}"
+  timeout      = "5s"
+  period       = "60s"
+
+  http_check {
+    path    = "/status"
+    port    = 443
+    use_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+
+    labels = {
+      host = "${var.uptime_check_host}"
+    }
+  }
+
+  content_matchers {
+    content = "OK"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_monitoring_alert_policy" "alert_uptime_errors_eq" {
+  project      = "${var.stackdriver_workspace}"
+  display_name = "Uptime health check"
+  combiner     = "OR"
+
+  conditions {
+    display_name = "Uptime health check"
+
+    condition_threshold {
+      filter          = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" AND resource.type=\"uptime_url\" AND metric.label.check_id=\"${basename(google_monitoring_uptime_check_config.http.id)}\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 1.0
+
+      trigger {
+        count = 1
+      }
+
+      aggregations {
+        alignment_period     = "1200s"
+        cross_series_reducer = "REDUCE_COUNT_FALSE"
+        group_by_fields      = ["resource.*"]
+        per_series_aligner   = "ALIGN_NEXT_OLDER"
+      }
+    }
+  }
+
+  notification_channels = [
+    "${google_monitoring_notification_channel.slack_alert.name}",
+  ]
+}
+
 resource "google_monitoring_alert_policy" "alert_500_errors_eq" {
   count        = "${var.stackdriver_workspace == "" ? 0 : 1}"
   project      = "${var.stackdriver_workspace}"
