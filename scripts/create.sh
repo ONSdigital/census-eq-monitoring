8fd212f41b995cacb34c56b96797b93d0b9e404f
James Gardiner <james@jgardiner.co.uk>
2020-01-20 13:00:44 +0000

    Aupdate task

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-set -ex
+set -euxo pipefail
 
 if [[ -z "$EXISTING_PROJECT_ID" ]]; then
   echo "Missing EXISTING_PROJECT_ID variable"
   exit 1
 fi
+
+tfenv use "$(< .terraform-version)"
 
 TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-census-eq-monitoring-tfstate}"
 
